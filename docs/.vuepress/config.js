bdda5c91485f2266ac821d84e65d78e04a2d5985
zhouxw <zhouxw@ehualu.com>
2021-11-07 12:44:52 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    title: 'ZhouXiaowan\'s blog ',
+    description: '我的个人网站',
+    themeConfig: {
+        nav:[ // 导航栏配置
+            {text: 'Github', link: 'https://github.com/zhouxiaowan'}
+        ],
+        sidebar: 'auto', // 侧边栏配置
+        sidebarDepth: 2, // 侧边栏显示2级
+    }
+
+}
